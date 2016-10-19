8146d9fd37898e3c6b9bd1315b7ec29ea78cde1d
Robert Mariano Schwindaman <schwindy1234@gmail.com>
2016-10-18 23:35:15 -0600

    API Fixes & Pool Methods Alpha

--- libs/poolWorker.js
+++ libs/poolWorker.js
@@ -175,25 +175,60 @@ module.exports = function(logger){
 
 
         var pool = Stratum.createPool(poolOptions, authorizeFN, logger);
-        pool.on('share', function(isValidShare, isValidBlock, data){
-
+        pool.on('share', function(isValidShare, isValidBlock, data)
+        {
             var shareData = JSON.stringify(data);
 
-            if (data.blockHash && !isValidBlock)
-                logger.debug(logSystem, logComponent, logSubCat, 'We thought a block was found but it was rejected by the daemon, share data: ' + shareData);
+            if(data.blockHash && !isValidBlock)
+                logger.debug
+                (
+                    logSystem,
+                    logComponent,
+                    logSubCat,
+                    'We thought a block was found but it was rejected by the daemon, share data: '+shareData
+                );
 
-            else if (isValidBlock)
-                logger.debug(logSystem, logComponent, logSubCat, 'Block found: ' + data.blockHash + ' by ' + data.worker);
+            else if(isValidBlock)
+                logger.debug
+                (
+                    logSystem, logComponent, logSubCat, 'Block found: ' + data.blockHash + ' by ' + data.worker);
 
-            if (isValidShare) {
+            if(isValidShare)
+            {
                 if(data.shareDiff > 1000000000)
-                    logger.debug(logSystem, logComponent, logSubCat, 'Share was found with diff higher than 1.000.000.000!');
+                    logger.debug
+                    (
+                        logSystem,
+                        logComponent,
+                        logSubCat,
+                        'Share was found with diff higher than 1.000.000.000!'
+                    );
                 else if(data.shareDiff > 1000000)
-                    logger.debug(logSystem, logComponent, logSubCat, 'Share was found with diff higher than 1.000.000!');
-                logger.debug(logSystem, logComponent, logSubCat, 'Share accepted at diff ' + data.difficulty + '/' + data.shareDiff + ' by ' + data.worker + ' [' + data.ip + ']' );
+                    logger.debug
+                    (
+                        logSystem,
+                        logComponent,
+                        logSubCat,
+                        'Share was found with diff higher than 1.000.000!'
+                    );
 
-            } else if (!isValidShare)
-                logger.debug(logSystem, logComponent, logSubCat, 'Share rejected: ' + shareData);
+                logger.debug
+                (
+                    logSystem,
+                    logComponent,
+                    logSubCat,
+                    'Share accepted at diff '+data.difficulty+' / '+data.shareDiff+' by '+data.worker+' ['+data.ip+']'
+                );
+
+            }
+            else if(!isValidShare)
+                logger.debug
+                (
+                    logSystem,
+                    logComponent,
+                    logSubCat,
+                    'Share rejected: '+shareData
+                );
 
             handlers.share(isValidShare, isValidBlock, data)
 
